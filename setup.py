30a0a404815e74227b11322ef19cd0f13393a03c
Kieran Wood <kieranw098@gmail.com>
2020-02-17 15:45:41 -0700

    Bumped Version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_content(*filename):
 
 setuptools.setup(
     name = "otp",
-    version = "1.0.1", 
+    version = "1.1.0", 
     author = "Kieran Wood", 
     author_email = "kieran@canadiancoding.ca",
     description = "Used to generate 🙊 one-time pads 🤐 exclusively in emojis.",
